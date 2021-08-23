28f95e3acc8927b54601722a627eba88c6ce0332
osmar-jr <osmar.junior@zup.com.br>
2021-08-23 17:17:57 -0300

    Login com Cartao via Keycloak

---
+++ src/main/java/br/com/zupacademy/osmarjunior/proposta/config/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.zupacademy.osmarjunior.proposta.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers(HttpMethod.GET, "/api/v1/propostas/**").hasAuthority("SCOPE_proposta-escopo")
+                                .antMatchers(HttpMethod.GET, "api/v1/acompanhar-proposta/**").hasAuthority("SCOPE_proposta-escopo")
+                                .antMatchers(HttpMethod.POST, "/api/v1/propostas").hasAuthority("SCOPE_proposta-escopo")
+                                .antMatchers(HttpMethod.POST, "/api/v1/biometrias").hasAuthority("SCOPE_proposta-escopo")
+                                .antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_proposta-escopo")
+                                .antMatchers(HttpMethod.POST, "/api/cartoes/").hasAuthority("SCOPE_proposta-escopo")
+                                .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
